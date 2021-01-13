49555611e035ee9134dff8af5adf3cf4bb8ac607
ltpbjo <ltpjob@163.com>
2021-01-13 11:14:19 +0800

    【mod】m3u8地址解析完成。

--- spider91.go
+++ spider91.go
@@ -1,21 +1,126 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
 	"regexp"
 	"src/github.com/PuerkitoBio/goquery"
+	"src/github.com/chromedp/chromedp"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func pageCrawl(url string) {
-	req, err := http.NewRequest("GET", url, nil)
+type VideoInfo struct {
+	Title   string
+	ViewKey string
+	upTime  time.Time
+	dlAddr  string
+}
+
+func (v VideoInfo) String() string {
+	return fmt.Sprintf("VideoInfo: %s %s %s %s", v.Title, v.ViewKey, v.upTime.Format("2006-01-02 15:04:05"), v.dlAddr)
+}
+
+func (v *VideoInfo) updateDlAddr() (err error) {
+
+	options := []chromedp.ExecAllocatorOption{
+		chromedp.Flag("hide-scrollbars", false),
+		chromedp.Flag("mute-audio", false),
+		chromedp.Flag("blink-settings", "imagesEnabled=false"),
+		//chromedp.Flag("headless", false),
+		//chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3830.0 Safari/537.36"),
+	}
+	options = append(chromedp.DefaultExecAllocatorOptions[:], options...)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), options...)
+	defer cancel()
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+	ctx, _ = context.WithTimeout(ctx, 60*time.Second)
+
+	htmlText := ""
+	fullUrl := "https://www.91porn.com/view_video.php?viewkey=" + v.ViewKey
+	if err = chromedp.Run(ctx, sourHtml(fullUrl, "#player_one_html5_api > source", &htmlText)); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(htmlText)
+	regAddr := regexp.MustCompile(`<source src="(?s:(.*?))" type="`)
+	dlAddr := regAddr.FindAllStringSubmatch(htmlText, 1)
+	if len(dlAddr) > 0 {
+		v.dlAddr = dlAddr[0][1]
+	}
+
+	return
+}
+
+func (v VideoInfo) Download(savePath string, numThread int) (err error) {
+
+	return err
+}
+
+func sourHtml(urlstr, sel string, html *string) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(urlstr),
+		//chromedp.WaitVisible(sel),
+		//chromedp.Text("source src", html),
+		//chromedp.ActionFunc(func(ctx context.Context) error {
+		//	return nil
+		//}),
+		chromedp.OuterHTML(sel, html),
+	}
+}
+
+func orgPageSave(dstUrl, proxyUrl, fileName string) {
+	req, err := http.NewRequest("GET", dstUrl, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9")
-	client := http.Client{}
+
+	proxy := func(_ *http.Request) (*url.URL, error) {
+		return url.Parse(proxyUrl)
+	}
+	transport := &http.Transport{Proxy: proxy}
+	client := &http.Client{Transport: transport}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	buf, _ := ioutil.ReadAll(resp.Body)
+	file.Write(buf)
+}
+
+func pageCrawl(dstUrl, proxyUrl string) (viAll []*VideoInfo) {
+	req, err := http.NewRequest("GET", dstUrl, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.Header.Add("Accept-Language", "zh-CN,zh;q=0.9")
+
+	proxy := func(_ *http.Request) (*url.URL, error) {
+		return url.Parse(proxyUrl)
+	}
+	transport := &http.Transport{Proxy: proxy}
+	client := &http.Client{Transport: transport}
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -49,12 +154,50 @@ func pageCrawl(url string) {
 		addTime := regAddTime.FindAllStringSubmatch(textStr, 1)
 
 		if len(viewkey) > 0 && len(addTime) > 0 && urlOk {
-			fmt.Println(title, viewkey[0][1], addTime[0][1])
+
+			vi := new(VideoInfo)
+
+			title = ""
+			strs := strings.Fields(addTime[0][1])
+
+			if len(strs) == 3 {
+				switch strs[1] {
+				case "分钟":
+					duration, _ := time.ParseDuration("-" + strs[0] + "m")
+					vi.upTime = time.Now().Add(duration)
+				case "小时":
+					duration, _ := time.ParseDuration("-" + strs[0] + "h")
+					vi.upTime = time.Now().Add(duration)
+				case "天":
+					hourDay, _ := strconv.Atoi(strs[0])
+					hourDay = hourDay * 24
+					duration, _ := time.ParseDuration("-" + strconv.Itoa(hourDay) + "h")
+					vi.upTime = time.Now().Add(duration)
+				}
+			}
+			vi.Title = title
+			vi.ViewKey = viewkey[0][1]
+			viAll = append(viAll, vi)
+			//fmt.Println(vi)
 		}
 
 	})
+	return
 }
 
 func main() {
-	pageCrawl("http://91porn.com/index.php")
+
+	//testUrl := "https://www.91porn.com/view_video.php?viewkey=16b541c59e6efb52e8dd"
+	const proxyUrl = "http://127.0.0.1:10808"
+	//testUrl = "https://www.google.com/"
+
+	viAll := pageCrawl("http://91porn.com/index.php", "http://127.0.0.1:10808")
+
+	//orgPageSave(testUrl, proxyUrl, "1.html")
+	for _, vi := range viAll {
+		vi.updateDlAddr()
+		fmt.Println(vi)
+	}
+
+	return
 }
