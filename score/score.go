c11086f6ca00e376422561cd413bb38a6b0af040
ltpbjo <ltpjob@163.com>
2021-01-20 15:03:55 +0800

    【mod】新加词汇全部来自文件。

--- score/score.go
+++ score/score.go
@@ -18,28 +18,6 @@ type Score struct {
 func NewScore(keyFile string) *Score {
 	mapKv := map[string]int{}
 	x := gojieba.NewJieba()
-	x.AddWord("真舒服")
-	x.AddWord("草死")
-	x.AddWord("网红")
-	x.AddWord("小女友")
-	x.AddWord("大屁股")
-	x.AddWord("跳蛋")
-	x.AddWord("女室友")
-	x.AddWord("D奶")
-	x.AddWord("d奶")
-	x.AddWord("E奶")
-	x.AddWord("e奶")
-	x.AddWord("F奶")
-	x.AddWord("f奶")
-	x.AddWord("小骚货")
-	x.AddWord("魔都")
-	x.AddWord("微露脸")
-	x.AddWord("干进去")
-	x.AddWord("肉臀")
-	x.AddWord("学霸")
-	x.AddWord("小母狗")
-	x.AddWord("高跟")
-	x.AddWord("00后")
 
 	data, err := ioutil.ReadFile(keyFile)
 	if err != nil {
