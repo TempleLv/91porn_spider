829f3109d47a734a74077fce019b4c103c3dd21c
ltpbjo <ltpjob@163.com>
2021-01-22 17:08:15 +0800

    【mod】使日常运行可以填入多个代理。

--- main.go
+++ main.go
@@ -17,6 +17,94 @@ import (
 	"time"
 )
 
+func weeklyFunc() func() {
+	return func() {
+
+		log.Println("Start weekly organize!!")
+		savePath := time.Now().Format("./save/weekly_060102")
+
+		path, _ := filepath.Abs(savePath)
+
+		_, err := os.Stat(path)
+		if os.IsNotExist(err) {
+			if err = os.MkdirAll(path, os.ModePerm); err != nil {
+				log.Println("savePath create failed!", err)
+				return
+			}
+		}
+		fi, _ := ioutil.ReadDir("./save")
+
+		for _, f := range fi {
+			if f.IsDir() && strings.Contains(f.Name(), "daily") {
+				os.Rename(filepath.Join("./save", f.Name()), filepath.Join(path, f.Name()))
+			}
+		}
+	}
+}
+
+func dailyFunc(proxyUrls []string) func() {
+	proxyUrls = append([]string{}, proxyUrls...)
+	return func() {
+		log.Println("Start daily Download!!")
+		var viAll []*catch.VideoInfo
+		s := score.NewScore("./score/wordValue.txt")
+		defer s.Free()
+	CRAWL:
+		for i := 1; i < 50; i++ {
+			var vis []*catch.VideoInfo
+			for _, pu := range proxyUrls {
+				vis = catch.PageCrawl("http://91porn.com/v.php?next=watch&page="+strconv.Itoa(i), pu)
+				if len(vis) > 0 {
+					break
+				}
+			}
+
+			for _, vi := range vis {
+				if time.Now().Sub(vi.UpTime) < time.Hour*24+time.Minute*10 {
+					viAll = append(viAll, vi)
+				} else {
+					break CRAWL
+				}
+			}
+		}
+
+		if len(viAll) > 0 {
+			s.GradeSort(viAll)
+			length := int(math.Min(40, float64(len(viAll))))
+			pickVi := viAll[:length]
+			savePath := time.Now().Format("./save/daily_060102")
+
+			path, _ := filepath.Abs(savePath)
+
+			_, err := os.Stat(path)
+			if os.IsNotExist(err) {
+				if err = os.MkdirAll(path, os.ModePerm); err != nil {
+					log.Println("savePath create failed!", err)
+					return
+				}
+			}
+
+			failVi := pickVi
+			for _, pu := range proxyUrls {
+				failVi = catch.DownloadMany(failVi, 5, pu, path)
+				if len(failVi) == 0 {
+					break
+				} else {
+					log.Printf("proxy:%s left %d items\n", pu, len(failVi))
+				}
+			}
+
+			log.Printf("Download total:%d, success %d, fail %d.\n", len(pickVi), len(pickVi)-len(failVi), len(failVi))
+			for _, vi := range failVi {
+				log.Println("Download Fail!", vi.Title, vi.ViewKey)
+			}
+		} else {
+			log.Println("No page was crawled!!!")
+		}
+
+	}
+}
+
 func main() {
 
 	proxyUrl := ""
@@ -63,73 +151,13 @@ func main() {
 
 	c := cron.New(cron.WithSeconds())
 
-	c.AddFunc("00 00 04 * * 6", func() {
+	c.AddFunc("00 00 04 * * 6", weeklyFunc())
 
-		log.Println("Start weekly organize!!")
-		savePath := time.Now().Format("./save/weekly_060102")
-
-		path, _ := filepath.Abs(savePath)
-
-		_, err := os.Stat(path)
-		if os.IsNotExist(err) {
-			if err = os.MkdirAll(path, os.ModePerm); err != nil {
-				log.Println("savePath create failed!", err)
-				return
-			}
-		}
-		fi, _ := ioutil.ReadDir("./save")
-
-		for _, f := range fi {
-			if f.IsDir() && strings.Contains(f.Name(), "daily") {
-				os.Rename(filepath.Join("./save", f.Name()), filepath.Join(path, f.Name()))
-			}
-		}
-	})
-
-	c.AddFunc("00 30 02 * * *", func() {
-		log.Println("Start daily Download!!")
-		var viAll []*catch.VideoInfo
-		s := score.NewScore("./score/wordValue.txt")
-		defer s.Free()
-	ALL:
-		for i := 1; i < 50; i++ {
-			vis := catch.PageCrawl("http://91porn.com/v.php?next=watch&page="+strconv.Itoa(i), "")
-			for _, vi := range vis {
-				if time.Now().Sub(vi.UpTime) < time.Hour*24+time.Minute*10 {
-					viAll = append(viAll, vi)
-				} else {
-					break ALL
-				}
-			}
-		}
-
-		s.GradeSort(viAll)
-		length := int(math.Min(40, float64(len(viAll))))
-		pickVi := viAll[:length]
-		savePath := time.Now().Format("./save/daily_060102")
-
-		path, _ := filepath.Abs(savePath)
-
-		_, err := os.Stat(path)
-		if os.IsNotExist(err) {
-			if err = os.MkdirAll(path, os.ModePerm); err != nil {
-				log.Println("savePath create failed!", err)
-				return
-			}
-		}
-
-		failVi := catch.DownloadMany(pickVi, 5, "", path)
-		if len(failVi) > 0 {
-			//use backup proxy to download
-			log.Printf("Download fail %d files,using backup proxy for saveing!\n", len(failVi))
-			failVi = catch.DownloadMany(pickVi, 5, "socks5://192.168.3.254:10808", path)
-		}
-
-		log.Printf("Download success %d files,fail %d files!\n", len(pickVi)-len(failVi), len(failVi))
-		for _, vi := range failVi {
-			log.Println("Download Fail!", vi.Title, vi.ViewKey)
-		}
-	})
+	proxyUrls := []string{
+		"",
+		"socks5://192.168.3.254:10808",
+	}
+	c.AddFunc("00 30 02 * * *", dailyFunc(proxyUrls))
 
 	c.Start()
 	defer c.Stop()
